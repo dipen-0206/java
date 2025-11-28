7fb66236625fd6cbcf0394a57789971c0307b56f
dipen-0206 <thapadipen88@gmail.com>
2025-11-28 10:12:55 +0545

    array

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -2,21 +2,36 @@ import java.util.*;
 public class ArrayExample {
     static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int []c=new int[]{1,2,3,4,5,6,7,8,9,10};
-        for (int x:c){
-            System.out.print(x+" ");
-        }
-        System.out.println();
+//        int []c=new int[]{1,2,3,4,5,6,7,8,9,10};
+//        for (int x:c){
+//            System.out.print(x+" ");
+//        }
+//        System.out.println();
+//
+//        int []d=new int[5];
+//        System.out.println("enter 5 number");
+//        for (int i=0;i<5;i++) {
+//            d[i] = sc.nextInt();
+//        }
+//        for (int x:d){
+//            System.out.print(x+" ");
+//        }
 
-        int []d=new int[5];
-        System.out.println("enter 5 number");
-        for (int i=0;i<5;i++) {
-            d[i] = sc.nextInt();
-        }
-        for (int x:d){
-            System.out.print(x+" ");
+
+        int [][]arr = new int[3][4];
+        System.out.println("Enter 3*4 array:");
+        for (int i=0;i<3;i++){
+            for (int j=0;j<4;j++){
+                arr[i][j]=sc.nextInt();
+            }
         }
 
+        for (int i=0;i<3;i++){
+            for (int j=0;j<4;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
 
 
 
