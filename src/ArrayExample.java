9560916b51a7bd72e475c7c9bb0576c7467e8558
dipen-0206 <thapadipen88@gmail.com>
2025-11-28 10:25:22 +0545

    rows array

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -18,21 +18,32 @@ public class ArrayExample {
 //        }
 
 
-        int [][]arr = new int[3][4];
-        System.out.println("Enter 3*4 array:");
-        for (int i=0;i<3;i++){
-            for (int j=0;j<4;j++){
-                arr[i][j]=sc.nextInt();
-            }
-        }
-
-        for (int i=0;i<3;i++){
-            for (int j=0;j<4;j++){
-                System.out.print(arr[i][j]+" ");
+//        int [][]arr = new int[3][4];
+//        System.out.println("Enter 3*4 array:");
+//        for (int i=0;i<3;i++){
+//            for (int j=0;j<4;j++){
+//                arr[i][j]=sc.nextInt();
+//            }
+//        }
+//
+//        for (int i=0;i<3;i++){
+//            for (int j=0;j<4;j++){
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+        int [][] arr=new int [][]{
+                {1},
+                {1,2},
+                {1,2,3},
+                {1,2,3,4}
+        };
+        for (int i=0;i<arr.length; i++){
+            for (int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]);
             }
             System.out.println();
         }
-
 
 
     }
