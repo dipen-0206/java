45efa07e009bb6e90f9a660c7a0967ebbb8cebb3
dipen-0206 <thapadipen88@gmail.com>
2025-12-04 01:41:46 +0545

    lab report

--- src/LabFirst.java
+++ src/LabFirst.java
@@ -2,6 +2,37 @@ import java.util.*;
 public class LabFirst {
     static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+//        System.out.println("enter P T R:");
+//        int P = sc.nextInt();
+//        int T = sc.nextInt();
+//        int R = sc.nextInt();
+//        int SI = (P*T*R)/100;
+//        System.out.println("The si ="+SI);
+
+//        System.out.println("enter numbers:");
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//        int s = a+b+c;
+//        int avg= s/3;
+//
+//        System.out.println("sum is "+s+ " average is "+avg);
+
+//        System.out.println("enter numbers:");
+//        int a=sc.nextInt();
+//        int b=sc.nextInt();
+//        int c=sc.nextInt();
+//        if (a>b && a>c){
+//            System.out.println(a+" is greatest");
+//        }
+//        else if (b>a && b>c) {
+//            System.out.println(b+" is greatest");
+//        }
+//        else
+//            System.out.println(c+" is greatest");
+
+
+
 
     }
 }
